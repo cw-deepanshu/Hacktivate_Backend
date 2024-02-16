6e61360747bef5da32a719f785239755b5702995
cw-aditi <aditi.s@carwale.com>
2024-02-16 12:22:26 +0530

    added analyser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const spawner = require('child_process').spawn;
 const multer = require("multer");
 const cors = require("cors");
 
@@ -12,12 +13,13 @@ const storage = multer.diskStorage({
         cb(null, "./uploads"); // Set your upload directory
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname); // Set filename
+        cb(null,file.originalname); // Set filename
     }
 });
 
 let upload = multer({
     storage: storage
+
 }).single("file");
 
 // Route to handle file upload
@@ -28,9 +30,25 @@ app.post("/upload", upload, (req, res) => {
 
     // Current language type
     const lang = req.body.language;
+    const file = req.file;
     console.log(lang);
+    console.log(file.originalname);
     // Respond with success message or do other processing
     res.send("File uploaded successfully.");
+
+
+    const data = [lang,file.originalname]
+    const python_process = spawner('python', ['Analyser\\main.py', JSON.stringify(data)]);
+    python_process.stdout.on('data', (data) => {
+        console.log(`Python Script Output: ${data}`);
+      });
+      python_process.stderr.on('data', (error) => {
+        console.error(`Error in Python Script: ${error}`);
+      });
+      
+      python_process.on('close', (code) => {
+        console.log(`Python Script Exited with Code: ${code}`);
+      });
 });
 
 const PORT = 5000;
